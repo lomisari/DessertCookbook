95429ddfdde88add61c41befc9bb4be286f48dcb
Nika Magradze <66585544+lomisari@users.noreply.github.com>
2023-05-25 19:50:39 -0500

    Initial Commit

---
+++ DessertApp/DessertAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertAppApp.swift
+//  DessertApp
+//
+//  Created by Nika Magradze on 5/25/23.
+//
+
+import SwiftUI
+
+@main
+struct DessertAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
